c476baf87f059a3fbd15f2e0c82fa7f9d893a79a
DrDronkaert <fourneaumanu@gmail.com>
2023-06-10 14:48:17 -0400

    initial styling and setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="full-width row wrap justify-center items-start content-center">
+    <div style="overflow: auto;" class="column content-center col-6">
+      <h5 style="text-align:center">Spinner</h5>
+      <div class="spinner"></div>
+    </div>
+    <div style="overflow: auto;" class="column content-center col-6">
+      <h5>Names</h5>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style lang="scss" scoped>
+.spinner {
+  width: 400px;
+  height: 400px;
+  background-image: conic-gradient(orange 64%, blue 17%, black);
+  border-radius: 50%
 }
 </style>
